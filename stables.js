5bf42f1494577c74b7c41cb355b2125e93f979f4
Justin <jmajam2001@gmail.com>
2025-10-09 16:47:16 -0700

    finished stables 2 work

--- stables.js
+++ stables.js
@@ -52,7 +52,7 @@
 // NOTE: UNLESS SPECIFIED, VARIABLE NAMING IS UP TO YOU. THERE SHOULD BE NO HARD-CODED
 // NUMBERS OR STRINGS WHERE VARIBALES WOULD BE PREFERRED.
 
-//------------------------- Seting up shop (Lab #1, Week 3) -------------------------//
+//------------------------- Seting up shop (Lab #1a, Week 3) -------------------------//
 
 // Declare variables with values for the following:
 // - the name of the horse at the stable
@@ -65,7 +65,7 @@
 // Create a variable and use it to store a message that monthly payment is late, and the amount owing.
 // Include the name of your horse in the message.
 
-//------------------------- First day (Lab #2, Week 4) -------------------------//
+//------------------------- First day (Lab #1b, Week 4) -------------------------//
 
 // Using an object, add at least 3 horses to your stables.
 //
@@ -83,6 +83,17 @@
 
 // Initialize new property to your horses: a boolean that allows you to check if your horse
 // is hungry or not. Use dot notation.
+
+//------------------------- Growing our business (Lab #2, Week 6) -------------------------//
+
+// Create a variable that stores your total number of stalls.
+// Create a function that logs out how many stalls are available, given how many horses you have in your stable.
+
+// Create a function that logs out how much will an individual horse will owe if rent is
+// paid late. Invoke the function.
+
+// Create and invoke a function that returns (not logs) the nickname of a chosen horse. Log out
+// the return value outside of the function.
 let horseName = "Nancy";
 let horseAge = 8;
 let horseIsInStable = false;
@@ -154,6 +165,28 @@ horses.horseOne.isHungry = true;
 horses.horseTwo.isHungry = true;
 horses.horseThree.isHungry = true;
 horses.horseFour.isHungry = true;
+
+//Lab 2
+let stallCount = 4;
+
+function printStallsAvailableStatement(stallCount, horsesInStable) {
+    let stallsAvailable = stallCount - horsesInStable;
+    console.log(
+        "There are " + stallsAvailable + " stalls available in the stable."
+    );
+}
+
+function printLateFeeStatement(horse, rent, lateFee) {
+    let total = rent + lateFee;
+    console.log(horse.name + "'s fee is late! You now owe $" + total);
+}
+
+function getHorseNickname(horse) {
+    return horse.name;
+}
+printStallsAvailableStatement(5, 2);
+printLateFeeStatement(horseTwo, monthlyBoardingCost, lateFee);
+console.log(getHorseNickname(horseOne));
 
 //notes
 //function declaration
