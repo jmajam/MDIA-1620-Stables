131e1bc0c47c55d86e76a75394ff270d30ed22f1
Justin <jmajam2001@gmail.com>
2025-10-16 10:23:18 -0700

    fixed getHorseNickname and commented unneeded code

--- stables.js
+++ stables.js
@@ -182,7 +182,7 @@ function printLateFeeStatement(horse, rent, lateFee) {
 }
 
 function getHorseNickname(horse) {
-    return horse.name;
+    return horse.nickname;
 }
 printStallsAvailableStatement(5, 2);
 printLateFeeStatement(horseTwo, monthlyBoardingCost, lateFee);
@@ -201,3 +201,16 @@ console.log(getHorseNickname(horseOne));
 //using variables as arguments is allowed
 //addTwo(testFirst,testSecond)
 //console.log(horses);
+
+let numberCubed = 5 * 5 * 5;
+let numberCubed2 = 2 * 2 * 2;
+let numberCubed3 = 4 * 4 * 4;
+
+function cubed(num) {
+    return num * num * num;
+}
+
+console.log(numberCubed);
+console.log(numberCubed2);
+
+console.log(cubed(1232434));
