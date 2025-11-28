6af8c19b022b2dfee50f36866a3606a26d67771d
Justin <jmajam2001@gmail.com>
2025-11-27 16:26:46 -0800

    redid show data structure in a comment

--- stables.js
+++ stables.js
@@ -1,194 +1,4 @@
 // Welcome!
-//
-//                                                  #    #
-//                                             %%% ##   ##
-//                                          %%%%% ###%🎀###
-//                                         %%%%% ### %%% #
-//                                       %%%%%% ### %%% ###
-//                                        %%%% ## %% #######
-//                                       %%%%% # %% #@#####
-//                                     %%%%%% # % #########
-//                                    %%%%% ##### #########
-//                          ###        %% ####### #########
-//                 %%% ############    ########### ########
-//              %%%% ############################### #######
-//            %%%%% ################################## ######
-//          %%%%%% #################################### #C###
-//         %%%🌸%% #####################################  ###
-//         %%%%% #######################################
-//        %%%%%% ############### BEANS ##################
-//     % %%%%%%% ############### BEANS ##################
-//      %%%%%%%%% ############## BEANS ##################
-//     %%%%%%%%%% ########################################
-//  %%% %%%%%%%%   ###### ################################
-//    %%%%%%%%      ###### #################### ##########
-// % %%%🌸%%%%        ####### ########### ###### ##########
-//  %%%%%%%%%         #######  ########### ###### ########
-// %%%%%%%%%%          ##### ###  ######### ####### ######
-//  %%%%%%%%%%          #### ##               ####### ####
-//  %🌸%%%%%%%%%           ## #                  ##### ###
-//   %%  %% % %%         # ##                      ## ###
-//     %   %    %        # ###                      # ###
-//                        # ###                     ## ###
-//                        # ###                     ## ###
-//                        # ####                   #### ##
-//                       ### ###                  ##### ###
-//                      ####  ###                 ####   ##
-//                     #####   ###                 ##    ##
-//                    #####    ####                      ###
-//                     ##        ###                     ###
-//                                ####                     ##
-//                                 ####                    ###
-//                                                         ####
-//                                                          ##
-//
-// This is a template for your labs and final project. You'll be setting up a stable and making
-// sure it runs smoothly.
-//
-// Please do not create a new file for each lab!
-// Feel free to keep the headings (e.g.: "Variables"), but please delete any boilerplate
-// comments. (Your own comments, where necessary, are acceptable.)
-//
-// NOTE: UNLESS SPECIFIED, VARIABLE NAMING IS UP TO YOU. THERE SHOULD BE NO HARD-CODED
-// NUMBERS OR STRINGS WHERE VARIBALES WOULD BE PREFERRED.
-
-//------------------------- Seting up shop (Lab #1a, Week 3) -------------------------//
-
-// Declare variables with values for the following:
-// - the name of the horse at the stable
-// - the age of the horse
-// - whether the horse is inside or outside
-// - the cost to board the horse monthly
-// - the fee for a late payment (monthly rate + 20%)
-//
-// Create a variable and use it to store a message for visitors to the stable.
-// Create a variable and use it to store a message that monthly payment is late, and the amount owing.
-// Include the name of your horse in the message.
-
-//------------------------- First day (Lab #1b, Week 4) -------------------------//
-
-// Using an object, add at least 3 horses to your stables.
-//
-// The horses should have the following properties:
-// - name, nickname, favorite treat (string)
-// - age, monthly rent (number)
-// - location (boolean inside/outside)
-// - two unique properties of your choice (use any primitive)
-
-// Store the horses you've just created in a "horses" variable.
-// Keep your old horse info from week one for now.
-
-// Fancy! Another horse wants to be stabled! Create a variable that stores an object
-// literal of your new horse, and add it to your "horses" variable.
-
-// Initialize new property to your horses: a boolean that allows you to check if your horse
-// is hungry or not. Use dot notation.
-
-//------------------------- Growing our business (Lab #2, Week 6) -------------------------//
-
-// Create a variable that stores your total number of stalls.
-// Create a function that logs out how many stalls are available, given how many horses you have in your stable.
-
-// Create a function that logs out how much will an individual horse will owe if rent is
-// paid late. Invoke the function.
-
-// Create and invoke a function that returns (not logs) the nickname of a chosen horse. Log out
-// the return value outside of the function.
-// let horseName = "Nancy";
-// let horseAge = 8;
-// let horseIsInStable = false;
-// let monthlyBoardingCost = 100;
-// let lateFee = monthlyBoardingCost * 0.2;
-// let visitorMessage = "Hello! Welcome to my stable!";
-// let latePaymentMessage =
-//     "Your monthly payment is late! You owe $" +
-//     (lateFee + monthlyBoardingCost) +
-//     ". " +
-//     horseName +
-//     " is now sad!";
-
-// // SEPT 25
-
-// let horseOne = {
-//     name: "Justin",
-//     nickname: "LongMan",
-//     favouriteTreat: "Cookies",
-//     age: 23,
-//     monthlyRent: 200,
-//     isInside: true,
-//     topSpeed: 20,
-//     fatigue: 100,
-// };
-
-// let horseTwo = {
-//     name: "Maple",
-//     nickname: "Zoomie",
-//     favouriteTreat: "Carrots",
-//     age: 17,
-//     monthlyRent: 180,
-//     isInside: false,
-//     topSpeed: 28,
-//     fatigue: 65,
-// };
-
-// let horseThree = {
-//     name: "Blaze",
-//     nickname: "ThunderHoof",
-//     favouriteTreat: "Apples",
-//     age: 9,
-//     monthlyRent: 220,
-//     isInside: true,
-//     topSpeed: 32,
-//     fatigue: 40,
-// };
-
-// let horses = {
-//     horseOne: horseOne,
-//     horseTwo: horseTwo,
-//     horseThree: horseThree,
-// };
-
-// let horseFour = {
-//     name: "Clover",
-//     nickname: "Breezy",
-//     favouriteTreat: "Mint Leaves",
-//     age: 12,
-//     monthlyRent: 190,
-//     isInside: false,
-//     topSpeed: 26,
-//     fatigue: 55,
-// };
-
-// horses.horseFour = horseFour;
-
-// horses.horseOne.isHungry = true;
-// horses.horseTwo.isHungry = true;
-// horses.horseThree.isHungry = true;
-// horses.horseFour.isHungry = true;
-
-// //Lab 2
-// let stallCount = 4;
-
-// function printStallsAvailableStatement(stallCount, horsesInStable) {
-//     let stallsAvailable = stallCount - horsesInStable;
-//     console.log(
-//         "There are " + stallsAvailable + " stalls available in the stable."
-//     );
-// }
-
-// function printLateFeeStatement(horse) {
-//     let total = horse.monthlyRent + horse.monthlyRent * 0.2;
-//     console.log(horse.name + "'s fee is late! You now owe $" + total + ".");
-// }
-
-// function getHorseNickname(horse) {
-//     return horse.nickname;
-// }
-// printStallsAvailableStatement(5, 2);
-// printLateFeeStatement(horseTwo);
-// console.log(getHorseNickname(horseOne));
-
-// Welcome!
 //        ||||||||||
 //       ||        ||
 //      ||  STABLE  ||
@@ -367,7 +177,60 @@ for (let i = 0; i < horses.length; i++) {
 }
 
 // Stable roster snapshot
-// console.log(horses);
+// [
+//   Horse {
+//     name: 'Thunder',
+//     nickname: 'Thundy',
+//     favouriteTreat: 'Carrots',
+//     age: 5,
+//     monthlyRent: 500,
+//     isInside: true,
+//     speed: 45,
+//     fatigue: 10,
+//     introduction: [Function (anonymous)],
+//     setSpeed: [Function (anonymous)],
+//     isHungry: false
+//   },
+//   Horse {
+//     name: 'Bella',
+//     nickname: 'Bell',
+//     favouriteTreat: 'Apples',
+//     age: 7,
+//     monthlyRent: 600,
+//     isInside: false,
+//     speed: 40,
+//     fatigue: 15,
+//     introduction: [Function (anonymous)],
+//     setSpeed: [Function (anonymous)],
+//     isHungry: false
+//   },
+//   Horse {
+//     name: 'Shadow',
+//     nickname: 'Shady',
+//     favouriteTreat: 'Sugar Cubes',
+//     age: 4,
+//     monthlyRent: 450,
+//     isInside: true,
+//     speed: 50,
+//     fatigue: 8,
+//     introduction: [Function (anonymous)],
+//     setSpeed: [Function (anonymous)],
+//     isHungry: false
+//   },
+//   {
+//     name: 'Storm',
+//     nickname: 'Stormy',
+//     favouriteTreat: 'Carrots',
+//     age: 6,
+//     monthlyRent: 550,
+//     isInside: true,
+//     speed: 42,
+//     fatigue: 12,
+//     introduction: [Function: introduction],
+//     setSpeed: [Function: setSpeed],
+//     isHungry: false
+//   }
+// ]
 
 stallsAvailable -= horses.length;
 
